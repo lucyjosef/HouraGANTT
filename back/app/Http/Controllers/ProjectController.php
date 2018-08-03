0cd8ba75807d6bb15999d85da37cb3f6bf38487c
Jeremie Soffichiti <jeremie.soffichiti@gmail.com>
2018-08-03 03:35:20 +0200

    Update function update project

--- back/app/Http/Controllers/ProjectController.php
+++ back/app/Http/Controllers/ProjectController.php
@@ -136,7 +136,17 @@ class ProjectController extends Controller
         }
         if(checkProjectRight($id, auth()->user()->id)) {
             if(checkRight(auth()->user()->id, $id)) {
-                DB::table('projects')->where('id', $id)->update($request[0]);
+                // MODIF JEREM
+                $project = Project::find($id);
+                $project->name = $request->name;
+                $project->description = $request->description;
+                $project->duration_days = $request->duration_days;
+                $project->link = $request->link;
+                $project->billing = $request->billing;
+                $project->save();
+
+
+                // DB::table('projects')->where('id', $id)->update($request);
                 return response()->json([
                     'status' => 'success',
                     'message' => 'Successfully updated',
